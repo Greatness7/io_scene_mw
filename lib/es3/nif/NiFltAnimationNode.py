92d5e6c979c31e11840c7b23694fda6bd9068d58
Greatness7 <Greatness7@gmail.com>
2020-05-19 01:16:12 -0400

    add access to bounce flag for NiFltAnimationNode

--- lib/es3/nif/NiFltAnimationNode.py
+++ lib/es3/nif/NiFltAnimationNode.py
@@ -1,10 +1,14 @@
 from __future__ import annotations
 
+from es3.utils.flags import bool_property
 from .NiSwitchNode import NiSwitchNode
 
 
 class NiFltAnimationNode(NiSwitchNode):
     period: float32 = 0
+
+    # flags access
+    bounce = bool_property(mask=0x0040)
 
     def load(self, stream):
         super().load(stream)
