5e7c66158191cd404c6d238b27986117083139aa
Greatness7 <Greatness7@gmail.com>
2020-08-04 12:32:55 -0400

    add NiLookAtController flip/axis

--- lib/es3/nif/NiLookAtController.py
+++ lib/es3/nif/NiLookAtController.py
@@ -1,12 +1,28 @@
 from __future__ import annotations
 
+from enum import IntEnum
+
+from es3.utils.flags import bool_property, enum_property
 from .NiTimeController import NiTimeController
+
+
+class Axis(IntEnum):
+    X = 0
+    Y = 1
+    Z = 2
 
 
 class NiLookAtController(NiTimeController):
     look_at: Optional[NiAVObject] = None
 
     _ptrs = (*NiTimeController._ptrs, "look_at")
+
+    # provide access to related enums
+    Axis = Axis
+
+    # convenience properties
+    flip = bool_property(mask=0x0010)
+    axis = enum_property(Axis, mask=0x0001, pos=5)
 
     def load(self, stream):
         super().load(stream)
