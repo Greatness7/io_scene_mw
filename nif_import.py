09ec46ebac5c6a4ece9726eb60d4e7a4305bf9f0
Greatness7 <Greatness7@gmail.com>
2021-06-01 01:29:06 -0400

    ensure created keyframes override default interpolation mode

--- nif_import.py
+++ nif_import.py
@@ -1339,11 +1339,12 @@ class Animation(SceneNode):
             for kp in fcurves.keyframe_points:
                 kp.interpolation = 'LINEAR'
         else:
+            for kp in fcurves.keyframe_points:
+                kp.interpolation = 'BEZIER'
+                kp.handle_left_type = kp.handle_right_type = 'FREE'
             handles = ni_data.get_tangent_handles()  # TODO: call this once per controller rather than per axis
             fcurves.keyframe_points.foreach_set("handle_left", handles[0, axis].ravel())
             fcurves.keyframe_points.foreach_set("handle_right", handles[1, axis].ravel())
-            for kp in fcurves.keyframe_points:
-                kp.handle_left_type = kp.handle_right_type = 'FREE'
 
     @staticmethod
     def update_frame_range(controller):
