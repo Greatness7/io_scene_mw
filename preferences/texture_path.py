40f168ea9b6c8b63c824e5aac9c6f88ad32852bf
Greatness7 <Greatness7@gmail.com>
2025-05-08 14:09:35 -0400

    Fix regression that caused errors when accessing the addon preferences.

--- preferences/texture_path.py
+++ preferences/texture_path.py
@@ -2,7 +2,7 @@ import bpy
 
 
 def addon_preferences(context):
-    return context.preferences.addons[__package__].preferences
+    return context.preferences.addons["io_scene_mw"].preferences
 
 
 class TexturePath(bpy.types.PropertyGroup):
