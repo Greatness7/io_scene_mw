d07d5f66d6a9549eeffe65bd71badc4b07acbdb8
Greatness7 <Greatness7@gmail.com>
2025-08-31 17:14:01 -0400

    Discard blender's numeric suffix from "Bounding Box" nodes.

--- __init__.py
+++ __init__.py
@@ -6,7 +6,7 @@ import bpy
 bl_info = {
     "name": "Morrowind (.nif)",
     "author": "Greatness7",
-    "version": (0, 8, 109),
+    "version": (0, 8, 110),
     "blender": (3, 6, 0),
     "location": "File > Import/Export > Morrowind (.nif)",
     "description": "Import/Export files for Morrowind",
