54eebd1aff911fefc4190683c8e3a137c9a6f81b
Greatness7 <Greatness7@gmail.com>
2026-02-26 02:54:17 -0500

    Bump version

--- __init__.py
+++ __init__.py
@@ -6,7 +6,7 @@ import bpy
 bl_info = {
     "name": "Morrowind (.nif)",
     "author": "Greatness7",
-    "version": (0, 8, 114),
+    "version": (0, 8, 116),
     "blender": (3, 6, 0),
     "location": "File > Import/Export > Morrowind (.nif)",
     "description": "Import/Export files for Morrowind",
